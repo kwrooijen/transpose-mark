2a06dd6f1ed2d4b591e8ceb0239b1575245df3ab
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2015-04-04 15:34:07 +0200

    Added overlay manipulation

--- transpose-mark.el
+++ transpose-mark.el
@@ -48,30 +48,10 @@
   (if (region-active-p) (transpose-mark-region) (transpose-mark-line)))
 
 ;;;###autoload
-(defun transpose-mark-region ()
-  "Transpose the current region with the previously marked region.
-Once you've transposed one the region is reset."
-  (interactive)
-  (if (and transpose-mark-region-overlay (overlay-start transpose-mark-region-overlay))
-      (let* ((current-region (buffer-substring-no-properties (mark) (point)))
-             (target-start (overlay-start transpose-mark-region-overlay))
-             (target-end (overlay-end transpose-mark-region-overlay))
-             (target-region (buffer-substring-no-properties target-start target-end)))
-        (if (> (mark) target-start)
-            (progn
-              (transpose-mark-region-set-current target-region)
-              (transpose-mark-region-set-target target-start target-end current-region))
-          (progn
-            (transpose-mark-region-set-target target-start target-end current-region)
-            (transpose-mark-region-set-current target-region)))
-        (delete-overlay transpose-mark-region-overlay))
-    (transpose-mark-save-point)))
-
-;;;###autoload
 (defun transpose-mark-line ()
   "Transpose the current line with the line which the current mark is pointing to."
   (interactive)
-  (delete-overlay transpose-mark-region-overlay)
+  (transpose-mark-delete-overlay)
   (let ((col (current-column)))
     (save-excursion
       (beginning-of-line)
@@ -84,6 +64,114 @@ Once you've transposed one the region is reset."
     (pop-mark)
     (move-to-column col)))
 
+;;;###autoload
+(defun transpose-mark-region ()
+  "Transpose the current region with the previously marked region.
+Once you've transposed one the region is reset."
+  (interactive)
+  (if (transpose-mark-region-overlay-active)
+      (let* ((current-region (buffer-substring-no-properties (mark) (point)))
+             (target-start (overlay-start transpose-mark-region-overlay))
+             (target-end (overlay-end transpose-mark-region-overlay))
+             (target-region (buffer-substring-no-properties target-start target-end)))
+        (if (> (mark) target-start)
+            (progn
+              (transpose-mark-region-set-current target-region)
+              (transpose-mark-region-set-target target-start target-end current-region))
+          (progn
+            (transpose-mark-region-set-target target-start target-end current-region)
+            (transpose-mark-region-set-current target-region)))
+        (transpose-mark-delete-overlay))
+    (transpose-mark-save-point)))
+
+;;;###autoload
+(defun transpose-mark-region-abort ()
+  "Remove the transpose-mark-region-overlay if set."
+  (interactive)
+  (transpose-mark-delete-overlay))
+
+;;;###autoload
+(defun transpose-mark-region-start-func (func)
+  "Run a function at the start of the overlay and expand the overlay selection to
+the new point after the function as been ran."
+  (interactive)
+  (if (transpose-mark-region-overlay-active)
+      (save-excursion
+        (let ((target-end (overlay-end transpose-mark-region-overlay)))
+          (goto-char (overlay-start transpose-mark-region-overlay))
+          (call-interactively func)
+          (transpose-mark-delete-overlay)
+          (setq-local transpose-mark-region-overlay (make-overlay (point) target-end))
+          (overlay-put transpose-mark-region-overlay 'face 'transpose-mark-region-set-face)))))
+
+;;;###autoload
+(defun transpose-mark-region-end-func (func)
+  "Run a function at the end of the overlay and expand the overlay selection to
+the new point after the function as been ran."
+  (interactive)
+  (if (transpose-mark-region-overlay-active)
+      (save-excursion
+        (let ((target-start (overlay-start transpose-mark-region-overlay)))
+          (goto-char (overlay-end transpose-mark-region-overlay))
+          (call-interactively func)
+          (transpose-mark-delete-overlay)
+          (setq-local transpose-mark-region-overlay (make-overlay target-start (point)))
+          (overlay-put transpose-mark-region-overlay 'face 'transpose-mark-region-set-face)))))
+
+;;;
+;;; Transpose Mark Region Expand Start Functions
+;;;
+
+;;;###autoload
+(defun tmr-start--forward-word ()
+  "Run transpose-mark-region-start-func with forward-word."
+  (interactive)
+  (transpose-mark-region-start-func 'forward-word))
+;;;###autoload
+(defun tmr-start--backward-word ()
+  "Run transpose-mark-region-start-func with backward-word."
+  (interactive)
+  (transpose-mark-region-start-func 'backward-word))
+;;;###autoload
+(defun tmr-start--forward-char ()
+  "Run transpose-mark-region-start-func with forward-char."
+  (interactive)
+  (transpose-mark-region-start-func 'forward-char))
+;;;###autoload
+(defun tmr-start--backward-char ()
+  "Run transpose-mark-region-start-func with backward-char."
+  (interactive)
+  (transpose-mark-region-start-func 'backward-char))
+
+;;;
+;;; Transpose Mark Region Expand End Functions
+;;;
+
+;;;###autoload
+(defun tmr-end--forward-word ()
+  "Run transpose-mark-region-end-func with forward-word."
+  (interactive)
+  (transpose-mark-region-end-func 'forward-word))
+;;;###autoload
+(defun tmr-end--backward-word ()
+  "Run transpose-mark-region-end-func with backward-word."
+  (interactive)
+  (transpose-mark-region-end-func 'backward-word))
+;;;###autoload
+(defun tmr-end--forward-char ()
+  "Run transpose-mark-region-end-func with forward-char."
+  (interactive)
+  (transpose-mark-region-end-func 'forward-char))
+;;;###autoload
+(defun tmr-end--backward-char ()
+  "Run transpose-mark-region-end-func with backward-char."
+  (interactive)
+  (transpose-mark-region-end-func 'backward-char))
+
+
+;;;
+;;; Private Functions
+;;;
 (defun transpose-mark-region-set-target (target-start target-end current-region)
   "Set the value of the target region."
   (kill-region target-start target-end)
@@ -106,6 +194,15 @@ Once you've transposed one the region is reset."
   (save-excursion
     (goto-char point)
     (insert string)))
+
+(defun transpose-mark-delete-overlay ()
+  "Delete transpose-mark-region-overlay if it exists."
+  (if (transpose-mark-region-overlay-active) (delete-overlay transpose-mark-region-overlay)))
+
+(defun transpose-mark-region-overlay-active ()
+  "Check if transpose-mark-region-overlay is active."
+  (and transpose-mark-region-overlay
+       (overlay-start transpose-mark-region-overlay)))
 
 (provide 'transpose-mark)
 ;;; transpose-mark.el ends here
